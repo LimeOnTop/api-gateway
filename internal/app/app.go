6019ed71b7502b37c5a4f642b9c8fdb042b2b5f9
LimeOnTop <k.cheglov1945@mail.ru>
2025-05-16 23:21:12 +0300

    add: CORS in app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -37,6 +37,20 @@ func NewServer(cfg *config.Config) (*Server, error) {
 
 	router := gin.Default()
 
+	// Добавьте CORS middleware
+    router.Use(func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    })
+
 	return &Server{
 		cfg:        cfg,
 		router:     router,
